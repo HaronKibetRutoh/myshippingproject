8cd5c98916b958991cdaaf08bdaf897028823b2a
Haron Kibet Rutoh <kibetharon7@gmail.com>
2020-08-11 16:47:04 +0300

    changed static root to assets

--- shipping/settings.py
+++ shipping/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT  = os.path.join(BASE_DIR, 'assets')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
